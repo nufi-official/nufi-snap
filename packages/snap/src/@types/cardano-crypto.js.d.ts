8a5908b2fae12b49a3527b8cbf76aa55b7348b86
PeterBenc <peter.benc1@gmail.com>
2023-10-13 14:03:22 +0200

    fix: remove unused deps

--- packages/snap/src/@types/cardano-crypto.js.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint import/unambiguous: 0 */
-
-// TODO: add proper types
-declare module 'cardano-crypto.js' {
-  export function derivePublic(...args: any[]): any;
-  export function mnemonicToRootKeypair(...args: any[]): any;
-}

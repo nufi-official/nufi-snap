86f80bb2a16c91a4d12a0e7f58bb654956fecc12
PeterBenc <peter.benc1@gmail.com>
2023-11-09 01:26:07 +0100

    refactor: be more explicit about the derivation paths we use

--- packages/snap/src/cardano/crypto-provider/key-utils.ts
+++ packages/snap/src/cardano/crypto-provider/key-utils.ts
@@ -1,6 +1,11 @@
 import * as cardanoCrypto from '@cardano-sdk/crypto';
 import { SLIP10Node } from '@metamask/key-tree';
 import { SupportedCardanoDerivationPath } from './types';
+import {
+  CARDANO_DERIVATION_PATH_COINTYPE,
+  CARDANO_DERIVATION_PATH_PURPOSE,
+  CARDANO_DERIVATION_PATH_VOTING_PURPOSE,
+} from './constants';
 
 /**
  * Converts a SLIP10Node to a Bip32PrivateKey.
@@ -41,15 +46,39 @@ async function getMetamaskAccountSLIP10Node([
   coinType,
   accountIndex,
 ]: SupportedCardanoDerivationPath): Promise<SLIP10Node> {
-  return SLIP10Node.fromJSON(
-    await snap.request({
-      method: 'snap_getBip32Entropy',
-      params: {
-        path: ['m', purpose, coinType, accountIndex],
-        curve: 'ed25519',
-      },
-    }),
-  );
+  if (
+    purpose === CARDANO_DERIVATION_PATH_VOTING_PURPOSE &&
+    coinType === CARDANO_DERIVATION_PATH_COINTYPE
+  ) {
+    return SLIP10Node.fromJSON(
+      await snap.request({
+        method: 'snap_getBip32Entropy',
+        params: {
+          path: ['m', "1694'", "1815'", accountIndex],
+          curve: 'ed25519',
+        },
+      }),
+    );
+  }
+
+  if (
+    purpose === CARDANO_DERIVATION_PATH_PURPOSE &&
+    coinType === CARDANO_DERIVATION_PATH_COINTYPE
+  ) {
+    return SLIP10Node.fromJSON(
+      await snap.request({
+        method: 'snap_getBip32Entropy',
+        params: {
+          path: ['m', "1852'", "1815'", accountIndex],
+          curve: 'ed25519',
+        },
+      }),
+    );
+  }
+
+  // purpose and coinType are strictly types so we could pass path directly to snap_getBip32Entropy and this throw is not needed
+  // but we want to be more explicit about the paths we use, so we check the purpose value and use literal values for path
+  throw new Error(`Unsupported purpose or coinType: ${purpose}, ${coinType}`);
 }
 
 /**

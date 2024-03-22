e9d4cea3c069d6f7996fd4e0e8190c6a1a4bc6fb
PeterBenc <peter.benc1@gmail.com>
2024-03-22 14:03:10 +0100

    refactor: move all validation utils to one file

--- packages/cardano-metamask-snap/src/api/requestValidation.ts
+++ packages/cardano-metamask-snap/src/api/requestValidation.ts
@@ -1,6 +1,5 @@
 import type { JsonRpcRequest } from '@metamask/snaps-sdk';
 
-import { isDerivationPath, isRecord, assertIsArray } from '../utils';
 import {
   CARDANO_DERIVATION_PATH_COINTYPE,
   CARDANO_DERIVATION_PATH_PURPOSE,
@@ -12,6 +11,50 @@ import type {
   SignTransactionRequestParams,
   SupportedCardanoDerivationPath,
 } from './types';
+
+/**
+ * Asserts that the given params is an array.
+ *
+ * @param params - The params to check.
+ * @throws If the params is not an array.
+ */
+export function assertIsArray(params: unknown): asserts params is unknown[] {
+  if (!Array.isArray(params)) {
+    throw new Error('Params must be an array');
+  }
+}
+
+/**
+ * Checks if the given param is an record.
+ *
+ * @param param - The param to check.
+ * @returns True if the param is an record, false otherwise.
+ */
+export function isRecord(param: unknown): param is Record<string, unknown> {
+  return Boolean(
+    param &&
+      typeof param === 'object' &&
+      Object.keys(param).every((key) => typeof key === 'string'),
+  );
+}
+
+/**
+ * Checks if the given param is a derivation path.
+ *
+ * @param path - The path to check.
+ * @returns True if the param is a derivation path, false otherwise.
+ */
+export function isDerivationPath(path: unknown): path is string[] {
+  return (
+    Array.isArray(path) &&
+    path.every((pathElement) => typeof pathElement === 'string') &&
+    path.every((pathElement) =>
+      pathElement.endsWith("'")
+        ? Number.isInteger(Number(pathElement.slice(0, -1)))
+        : Number.isInteger(Number(pathElement)),
+    )
+  );
+}
 
 /**
  * Checks if the given derivation path is of supported type.

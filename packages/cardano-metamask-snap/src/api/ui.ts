bf138d16ed0282d240fbaee592c3a51cf19af5bc
PeterBenc <peter.benc1@gmail.com>
2024-04-10 14:48:17 +0200

    fix: use only one divider for section

--- packages/cardano-metamask-snap/src/api/ui.ts
+++ packages/cardano-metamask-snap/src/api/ui.ts
@@ -1,7 +1,4 @@
 import { panel, divider } from '@metamask/snaps-sdk';
 
 export const section = (args: typeof panel.arguments) =>
-  panel([divider(), divider(), ...args]);
-
-export const subSection = (args: typeof panel.arguments) =>
   panel([divider(), ...args]);

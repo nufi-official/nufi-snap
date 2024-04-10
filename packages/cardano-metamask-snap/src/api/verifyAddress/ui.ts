a2321d104da6ad2de1b8240179b2d84922cfcc58
PeterBenc <peter.benc1@gmail.com>
2024-04-10 14:26:12 +0200

    fix: be consistent with hw wallet and number accounts from 1

--- packages/cardano-metamask-snap/src/api/verifyAddress/ui.ts
+++ packages/cardano-metamask-snap/src/api/verifyAddress/ui.ts
@@ -1,7 +1,19 @@
 import { panel, copyable, heading, text, row } from '@metamask/snaps-sdk';
 
 import type { VerifyAddressRequestParams } from '.';
+import type { CardanoDerivationPath } from '../derivationPath';
 import { section } from '../ui';
+
+const renderAddressAccountIndex = (
+  accountAddressIndex: CardanoDerivationPath[2] | null,
+) => {
+  if (!accountAddressIndex) {
+    return '';
+  }
+  // hw wallets also number accounts from 1
+  const accountIndex = Number(accountAddressIndex.slice(0, -1)) + 1;
+  return `account #${accountIndex}`;
+};
 
 export const renderVerifyAddress = async (
   param: VerifyAddressRequestParams[number],
@@ -13,17 +25,12 @@ export const renderVerifyAddress = async (
     VerifyAddressRequestParams[number]['addressType'],
     string
   >,
-  accountAddressIndex: `${number}'` | null,
+  accountAddressIndex: CardanoDerivationPath[2] | null,
   address: string,
 ) => {
-  const headingText = `Verify ${
-    accountAddressIndex
-      ? `account #${accountAddressIndex.slice(
-          0,
-          accountAddressIndex.length - 1,
-        )}`
-      : ''
-  } ${addressDescriptionForType[param.addressType]} address`;
+  const headingText = `Verify ${renderAddressAccountIndex(
+    accountAddressIndex,
+  )} ${addressDescriptionForType[param.addressType]} address`;
 
   const addressUiElements = section([
     row('Network', text(`${networkNameForId[param.networkId]}`)),

466a6bf1e90f49062890594a3485bba35147cc36
PeterBenc <peter.benc1@gmail.com>
2024-04-16 22:59:03 +0200

    feat: show message also in utf8

--- packages/cardano-metamask-snap/src/api/signData/ui.ts
+++ packages/cardano-metamask-snap/src/api/signData/ui.ts
@@ -1,4 +1,6 @@
 import { text, copyable, heading, panel } from '@metamask/snaps-sdk';
+// eslint-disable-next-line import/no-nodejs-modules
+import { Buffer } from 'buffer';
 
 import { section } from '../ui';
 
@@ -6,6 +8,8 @@ export const renderSignData = async (payloadHex: string) => {
   const headingText = 'Sign message';
 
   const messageUiElements = section([
+    text('Message:'),
+    copyable(Buffer.from(payloadHex, 'hex').toString('utf-8')),
     text('Raw message:'),
     copyable(payloadHex),
   ]);

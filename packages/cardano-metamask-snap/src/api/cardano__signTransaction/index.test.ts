5d120bef46998ff7b58bf52c52b31cd223e47841
PeterBenc <peter.benc1@gmail.com>
2024-04-22 12:51:28 +0200

    fix: transaction fixtures

--- packages/cardano-metamask-snap/src/api/cardano__signTransaction/index.test.ts
+++ packages/cardano-metamask-snap/src/api/cardano__signTransaction/index.test.ts
@@ -1,46 +1,64 @@
 import { expect } from '@jest/globals';
 import { installSnap } from '@metamask/snaps-jest';
 
+import { accountFixture } from '../../fixtures/accountFixture';
 import { origin } from '../../fixtures/constants';
 import { transactionsFixture } from '../../fixtures/transactionsFixtures';
 
-describe('cardano__signTransaction', () => {
-  it('should sign transaction', async () => {
-    const { request } = await installSnap();
-
-    const txType = 'simple';
-
-    const { txCborHex, txBodyHashHex, witnesses } = transactionsFixture[txType];
-
-    const pendingResponse = request({
-      method: 'cardano__signTransaction',
-      origin,
-      params: [
-        {
-          txCborHex,
-          derivationPaths: witnesses.map(
-            ({ derivationPath }) => derivationPath,
-          ),
-        },
-      ],
-    });
-
-    const ui = await pendingResponse.getInterface();
-    await ui.ok();
-
-    const { response: actualResponse } = await pendingResponse;
-
-    const expectedResponse = {
-      result: {
-        txBodyHashHex,
-        witnesses,
+const fixtures = {
+  simple: {
+    ...transactionsFixture.simple,
+    witnesses: [
+      {
+        ...accountFixture.paymentPart,
+        signatureHex:
+          'b7b0c4489155f9b279f2911d0f002830d10427f4ec7cc3c7969e0f0f91b8f723bef2b86be9c5d0c2ca65a29ee1b196a2bedeea522f097915613c303a2ef4700a',
       },
-    };
+      {
+        ...accountFixture.stakePart,
+        signatureHex:
+          'afe5c9af375ed8ad1d5d499aba59c8261645707572102c7efad45e9cfc189cdbf33279301b26fd471ecb423d3ccf185a723da1934fc9b0c011a34f507b542209',
+      },
+    ],
+  },
+};
 
-    expect(JSON.stringify(actualResponse)).toStrictEqual(
-      JSON.stringify(expectedResponse),
-    );
-  });
+describe('cardano__signTransaction', () => {
+  Object.entries(fixtures).forEach(
+    ([txType, { txCborHex, txBodyHashHex, witnesses }]) =>
+      it(`should sign ${txType} transaction`, async () => {
+        const { request } = await installSnap();
+
+        const pendingResponse = request({
+          method: 'cardano__signTransaction',
+          origin,
+          params: [
+            {
+              txCborHex,
+              derivationPaths: witnesses.map(
+                ({ derivationPath }) => derivationPath,
+              ),
+            },
+          ],
+        });
+
+        const ui = await pendingResponse.getInterface();
+        await ui.ok();
+
+        const { response: actualResponse } = await pendingResponse;
+
+        const expectedResponse = {
+          result: {
+            txBodyHashHex,
+            witnesses,
+          },
+        };
+
+        expect(JSON.stringify(actualResponse)).toStrictEqual(
+          JSON.stringify(expectedResponse),
+        );
+      }),
+  );
 
   it('should fail for unsupported path', async () => {
     const { request } = await installSnap();

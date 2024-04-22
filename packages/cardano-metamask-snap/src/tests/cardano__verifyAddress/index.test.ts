a077b3e9c6ec907bae5aa1b63bc68c1a5a431595
PeterBenc <peter.benc1@gmail.com>
2024-04-22 11:11:33 +0200

    fix: use stringified ui for verify address tests

--- packages/cardano-metamask-snap/src/tests/cardano__verifyAddress/index.test.ts
+++ packages/cardano-metamask-snap/src/tests/cardano__verifyAddress/index.test.ts
@@ -1,13 +1,27 @@
 import { expect } from '@jest/globals';
 import { installSnap } from '@metamask/snaps-jest';
-import type { Copyable } from '@metamask/snaps-sdk';
 
 import { accountFixture } from '../accountFixture';
 import { origin } from '../constants';
 
+const fixtures = {
+  basePaymentKeyStakeKeyAddress: {
+    ...accountFixture.addresses.basePaymentKeyStakeKeyAddress,
+    stringifiedUi: `{"type":"alert","content":{"type":"panel","children":[{"type":"heading","value":"Verify account #1  address"},{"type":"panel","children":[{"type":"divider"},{"type":"row","label":"Network","value":{"type":"text","value":"Mainnet"}},{"type":"row","label":"Payment path","value":{"type":"text","value":"1852'/1815'/0'/0/0"}},{"type":"row","label":"Stake path","value":{"type":"text","value":"1852'/1815'/0'/2/0"}},{"type":"copyable","value":"addr1qxgcuk5j0q0k2d0s9axvah49aut4ct5a5ertwp67psz3uuejm6ernk539y4mwwzrmny7ducc4d50mf6jfqvu79ghryss0cc0r2"}]}]}}`,
+  },
+  enterpriseKeyAddress: {
+    ...accountFixture.addresses.enterpriseKeyAddress,
+    stringifiedUi: `{"type":"alert","content":{"type":"panel","children":[{"type":"heading","value":"Verify account #1 enterprise address"},{"type":"panel","children":[{"type":"divider"},{"type":"row","label":"Network","value":{"type":"text","value":"Mainnet"}},{"type":"row","label":"Payment path","value":{"type":"text","value":"1852'/1815'/0'/0/0"}},{"type":"copyable","value":"addr1vxgcuk5j0q0k2d0s9axvah49aut4ct5a5ertwp67psz3uucl8y23r"}]}]}}`,
+  },
+  rewardKeyAddress: {
+    ...accountFixture.addresses.rewardKeyAddress,
+    stringifiedUi: `{"type":"alert","content":{"type":"panel","children":[{"type":"heading","value":"Verify account #1 rewards address"},{"type":"panel","children":[{"type":"divider"},{"type":"row","label":"Network","value":{"type":"text","value":"Mainnet"}},{"type":"row","label":"Stake path","value":{"type":"text","value":"1852'/1815'/0'/2/0"}},{"type":"copyable","value":"stake1uyedav3em2gjj2ah8ppaej0x7vv2k68a5afysxw0z5t3jgg59p6yp"}]}]}}`,
+  },
+};
+
 describe('cardano__verifyAddress', () => {
-  Object.entries(accountFixture.addresses).forEach(
-    ([addressType, { addressParams, bech32Address, networkId }]) =>
+  Object.entries(fixtures).forEach(
+    ([addressType, { addressParams, networkId, stringifiedUi }]) =>
       it(`should display copyable address for ${addressType} `, async () => {
         const { request } = await installSnap();
 
@@ -23,17 +37,8 @@ describe('cardano__verifyAddress', () => {
         });
 
         const ui = await pendingResponse.getInterface();
-        const copyableValue = (
-          ui.content.type === 'panel' &&
-          ui.content.children[1]?.type === 'panel'
-            ? ui.content.children[1].children.find(
-                (element): element is Copyable => element.type === 'copyable',
-              )
-            : undefined
-        )?.value;
-        await ui.ok();
 
-        expect(copyableValue).toBe(bech32Address);
+        expect(JSON.stringify(ui)).toBe(stringifiedUi);
       }),
   );
 

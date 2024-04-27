9ba8be71f973af78db7ad0e442b985795a541317
PeterBenc <peter.benc1@gmail.com>
2024-04-27 23:30:52 +0200

    fix: rename derivation path to witnessKeyPath:

--- packages/cardano-metamask-snap/src/api/cardano__signTransaction/index.test.ts
+++ packages/cardano-metamask-snap/src/api/cardano__signTransaction/index.test.ts
@@ -10,12 +10,14 @@ const fixtures = {
     ...transactionsFixture.simple,
     witnesses: [
       {
-        ...accountFixture.paymentPart,
+        witnessKeyPath: accountFixture.paymentPart.derivationPath,
+        extendedPublicKeyHex: accountFixture.paymentPart.extendedPublicKeyHex,
         signatureHex:
           'b7b0c4489155f9b279f2911d0f002830d10427f4ec7cc3c7969e0f0f91b8f723bef2b86be9c5d0c2ca65a29ee1b196a2bedeea522f097915613c303a2ef4700a',
       },
       {
-        ...accountFixture.stakePart,
+        witnessKeyPath: accountFixture.stakePart.derivationPath,
+        extendedPublicKeyHex: accountFixture.stakePart.extendedPublicKeyHex,
         signatureHex:
           'afe5c9af375ed8ad1d5d499aba59c8261645707572102c7efad45e9cfc189cdbf33279301b26fd471ecb423d3ccf185a723da1934fc9b0c011a34f507b542209',
       },
@@ -29,7 +31,8 @@ const fixtures = {
     ...transactionsFixture.simpleWithChangeOutput,
     witnesses: [
       {
-        ...accountFixture.paymentPart,
+        witnessKeyPath: accountFixture.paymentPart.derivationPath,
+        extendedPublicKeyHex: accountFixture.paymentPart.extendedPublicKeyHex,
         signatureHex:
           '1e66e1dd9b4d808109a8217cb7d76869cebf34a4bbb9c13fd2cad7cf5c2f0f628166d452d7ec53b656f1b4fc50a6d06f9e902720584414ca2d8f2feb69a7a506',
       },
@@ -71,8 +74,8 @@ describe('cardano__signTransaction', () => {
               txCborHex,
               networkId,
               changeOutputsParams,
-              derivationPaths: witnesses.map(
-                ({ derivationPath }) => derivationPath,
+              witnessKeysPaths: witnesses.map(
+                ({ witnessKeyPath }) => witnessKeyPath,
               ),
             },
           ],
@@ -108,7 +111,7 @@ describe('cardano__signTransaction', () => {
       params: [
         {
           txBodyHashHex: 'deadbeef',
-          derivationPaths: [derivationPath],
+          witnessKeysPaths: [derivationPath],
         },
       ],
     });

4e01505afb371d69f7c3aa6de2e54c2027f578aa
PeterBenc <peter.benc1@gmail.com>
2024-05-17 16:11:26 +0200

    refactor: move account index utils out of cardano__verifyAddress

--- packages/cardano-metamask-snap/src/api/ui.ts
+++ packages/cardano-metamask-snap/src/api/ui.ts
@@ -1,7 +1,20 @@
 import { panel, divider } from '@metamask/snaps-sdk';
 
+import type { CardanoDerivationPath } from './derivationPath';
+
 export const section = (args: typeof panel.arguments) =>
   panel([divider(), divider(), ...args]);
 
 export const subSection = (args: typeof panel.arguments) =>
   panel([divider(), ...args]);
+
+export const getUiAccountIndex = (
+  accountIndex: CardanoDerivationPath[2] | null,
+) => {
+  if (!accountIndex) {
+    return '';
+  }
+  // hw wallets also number accounts from 1
+  const uiAccountIndex = Number(accountIndex.slice(0, -1)) + 1;
+  return `account #${uiAccountIndex}`;
+};
